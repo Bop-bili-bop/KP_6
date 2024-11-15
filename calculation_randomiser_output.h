92ec265f8ca13d5c0d1e8dcc1383459c054a9790
Bop-bili-bop <kisurin.kyrylo@lll.kpi.ua>
2024-11-15 21:00:08 +0200

    fix output, double random generator

--- calculation_randomiser_output.h
+++ calculation_randomiser_output.h
@@ -28,21 +28,28 @@ void calculate_slae(double **a, double *b, double *xp, double *x, int rows, doub
     }
     while (max_delta < epsilon);
 }
-void generate_random_a_elements(double min, double max, double **arr, int rows, double epsilon)
+void generate_random_a_elements(double min, double max, double **arr, int rows)
 {
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < rows; j++)
         {
-            arr[i][j] = min + rand() / (double)RAND_MAX * (max - min) + min + rand() / (double)RAND_MAX * (max - min) / log10(epsilon);
+            arr[i][j] = (double)rand() + (rand() + (rand() / (double)RAND_MAX)) / (RAND_MAX + 1.0);
         }
     }
 }
-void generate_random_b_elements(double min, double max, double *arr, int rows, double epsilon)
+void generate_random_b_elements(double min, double max, double *arr, int rows)
 {
     for (int i = 0; i < rows; i++)
     {
-        arr[i]= min + (rand() / (double)RAND_MAX) * (max - min) + min + rand() / (double)RAND_MAX * (max - min) / log10(epsilon);
+        arr[i]= (double)rand() + (rand() + (rand() / (double)RAND_MAX)) / (RAND_MAX + 1.0);
+    }
+}
+void print_elements(char *label, double *arr, int rows, int precision)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        printf("%s[%d] = %.*lf\n", label, i+1, precision, arr[i]);
     }
 }
 #endif
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "calculation.h"
+#include "calculation_randomiser_output.h"
 #include "validation.h"
 #include <time.h>
 #include <conio.h>
@@ -10,12 +10,13 @@ int main()
     do
     {
         srand(time(NULL));
-        int rows = validate_int_input("Enter n size of SLAE\n", 1, 3200);
+        int rows = validate_int_input("Enter n size of SLAE\n", 1, 10);
         double epsilon = validate_double_input("Enter precision epsilon:\n", 1e-15, 1e-1);
         double *b = calloc(rows, sizeof(double));
         double *x = calloc(rows, sizeof(double));
         double *xp = calloc(rows, sizeof(double));
         double **a = calloc(rows, sizeof(double *));
+        int precision = fabs(log10(epsilon));
         for (int i = 0; i < rows; i++)
         {
             a[i] = calloc(rows, sizeof(double));
@@ -35,27 +36,23 @@ int main()
                     b[i] = validate_double_input("[-2147483646, 2147483646]: ", -2147483646, 2147483646);
                 }
             case 'r':
-                generate_random_a_elements(-2147483646.0, 2147483646.0, a, rows, epsilon);
-            generate_random_b_elements(-2147483646.0, 2147483646.0, b, rows, epsilon);
+                generate_random_a_elements(-2147483646.0, 2147483646.0, a, rows);
+            generate_random_b_elements(-2147483646.0, 2147483646.0, b, rows);
             default:
         }
-        printf(">>-----------------------------------------*-----------------------------------------<<\n");
+        printf("\n");
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < rows; j++)
             {
-                printf("%lf ||\t", a[i][j]);
+                printf("||\t%.*lf\t", precision, a[i][j]);
             }
             printf("\n");
         }
-
-        printf(">>-----------------------------------------*-----------------------------------------<<\n");
+        print_elements("b", x, rows, precision);
+        printf("\n");
         calculate_slae(a, b, xp, x , rows, epsilon);
-
-        for (int i = 0; i < rows; i++)
-        {
-            printf("x[%d] = %.*lf\n", i+1, (int)fabs(log10(epsilon)), x[i]);
-        }
+        print_elements("x", x ,rows, precision );
         //freeing memory
         free(xp);
         free(x);
@@ -65,7 +62,7 @@ int main()
             free(a[i]);
         }
         free(a);
-        printf("Press q to quit, any other key to continue...");
+        printf("Press q to quit, any other key to continue...\n");
     }
     while (getch() != 'q');
     return 0;
